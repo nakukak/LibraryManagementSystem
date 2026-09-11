5454be51e90f78d5172c79cab815326386b3b89e
Tom <nakukak@gmail.com>
2026-09-11 11:42:05 +0200

    Onsdag

---
+++ src/Member.java
@@ -0,0 +1,20 @@
+public class Member {
+    final String name;
+    final int id;
+
+
+    public Member(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String toString() {
+        return String.format("""
+                %s (Medlems nummer; %d)
+                """, name, id
+        );
+    }
+}
+
+
+//
